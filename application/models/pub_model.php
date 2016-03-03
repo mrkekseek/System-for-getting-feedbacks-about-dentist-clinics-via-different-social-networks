bc4706958dd5039741e7b94450ad9a8c7af546d4
Nick van den Berg <development@cloudrocket.co>
2016-03-03 15:17:13 +0100

    gitignore updated

--- application/models/pub_model.php
+++ application/models/pub_model.php
@@ -3014,16 +3014,16 @@
 				$config['newline'] = "\r\n";
 				$config['crlf'] = "\r\n";
 				
-				/*$config['protocol'] = 'smtp';
-				$config['smtp_user'] = 'postmaster@mg.patientenreview.nl';
-				$config['smtp_pass'] = '4add968a0a17e23db8752874334c9ae2';
-				$config['smtp_host'] = 'ssl://smtp.mailgun.org';
-				$config['smtp_port'] = '465';
-				$config['smtp_timeout'] = '4';
-				$config['charset'] = 'utf-8';
-				$config['mailtype'] = 'html';
-				$config['newline'] = "\r\n";
-				$config['crlf'] = "\n";*/
+				// $config['protocol'] = 'smtp';
+				// $config['smtp_user'] = 'postmaster@mg.patientenreview.nl';
+				// $config['smtp_pass'] = '4add968a0a17e23db8752874334c9ae2';
+				// $config['smtp_host'] = 'ssl://smtp.mailgun.org';
+				// $config['smtp_port'] = '465';
+				// $config['smtp_timeout'] = '4';
+				// $config['charset'] = 'utf-8';
+				// $config['mailtype'] = 'html';
+				// $config['newline'] = "\r\n";
+				// $config['crlf'] = "\n";
 
 				$this->load->library('email', $config);
 
