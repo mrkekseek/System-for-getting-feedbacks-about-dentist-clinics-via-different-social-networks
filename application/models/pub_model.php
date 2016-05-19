73d04790cdca59f7caecb8155de2b7553461baf8
Ievgenii Domanskyi <info@div-art.com>
2016-05-19 13:06:19 +0300

    Show max 10 rows

--- application/models/pub_model.php
+++ application/models/pub_model.php
@@ -4947,6 +4947,7 @@
 					
 					$this->db->order_by('sent_date', 'desc');
 					$this->db->where('users_id', $users_id);
+					$this->db->limit(10);
 					$result = $this->db->get('sent_dates')->result_array();
 					foreach ($result as $row)
 					{
